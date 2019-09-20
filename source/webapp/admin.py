09450d575e26a03cb2efdab4d8dd20706b3a4261
Kutman <muctep.k@gmail.com>
2019-09-20 20:19:47 +0600

    Добавлена модель Продукт (Product), описаны его поля. Модель добавлена в админку.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'category', 'remain', 'price']
+    list_display_links = ['name']
+    list_filter = ['category']
+    search_fields = ['name', 'description']
+    fields = ['name', 'description', 'category', 'remain', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = [
+    ('other', 'Разное'),
+    ('monitor', 'Мониторы'),
+    ('hdd', 'Жесткие диски'),
+    ('keyboard', 'Клавиатуры'),
+    ('mouse', 'Мышки'),
+    ('cpu', 'Центральные процессоры')
+]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Название')
+    description = models.TextField(max_length=2000, verbose_name='Описание')
+    category = models.CharField(max_length=50, null=False, blank=False, default=CATEGORY_CHOICES[0][0],
+                                choices=CATEGORY_CHOICES, verbose_name='Категория')
+    remain = models.PositiveIntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Стоимость')
+
+    def __str__(self):
+        return self.name
