12558d49b89fa2df5529305e0919c0b663b8e180
Kutman <muctep.k@gmail.com>
2019-09-24 14:19:05 +0600

    Добавлена главная страница

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from webapp.models import Product, CATEGORY_CHOICES
 
-# Create your views here.
+
+def index_view(request):
+    products = Product.objects.all().exclude(remain=0).order_by('category', 'name')
+    context = {'products': products}
+    return render(request, 'index.html', context)
