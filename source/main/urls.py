1c3d393b0ce6c2433872cfce8fb1e32af63d1755
Kutman <muctep.k@gmail.com>
2019-09-24 14:22:36 +0600

    Добавлена возможность просмотра отдельного товара

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view
+from webapp.views import index_view, product_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name='index')
+    path('', index_view, name='index'),
+    path('product/<int:pk>', product_detail_view, name='product_detail')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from webapp.models import Product, CATEGORY_CHOICES
 
 
@@ -6,3 +6,10 @@ def index_view(request):
     products = Product.objects.all().exclude(remain=0).order_by('category', 'name')
     context = {'products': products}
     return render(request, 'index.html', context)
+
+
+def product_detail_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product.html', context={
+        'product': product
+    })
